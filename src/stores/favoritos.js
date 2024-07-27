3ed5216d4b634a274e5e8c5d0c809b52080ef979
Cristian Sena <crisianjsena@gmail.com>
2024-07-27 17:56:58 -0300

    refactor agregar/eliminar

--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -2,10 +2,13 @@ import { ref, watch, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { defineStore } from 'pinia'
 import { useBebidasStore } from './bebidas'
+import { useModalStore } from './modal'
 
 export const useFavoritosStore = defineStore('favoritos', () => {
 
     const bebidas = useBebidasStore()
+    const modal = useModalStore()
+
     const favoritos = ref([])
 
     const router = useRouter()
@@ -18,18 +21,30 @@ export const useFavoritosStore = defineStore('favoritos', () => {
         sincronizarLocalStorage()
     }, { deep: true })
 
-    const sincronizarLocalStorage = () => {
+    function sincronizarLocalStorage() {
         localStorage.setItem('favoritos', JSON.stringify(favoritos.value))
     }
 
-    function handleClickFavorito() {
+    function existeFavorito(id) {
+        const favoritosLocalStorage = JSON.parse(localStorage.getItem('favoritos')) ?? []
+        return favoritosLocalStorage.some(favorito => favorito.idDrink === id)
+    }
+
+    function agregarFavorito() {
         favoritos.value.push(bebidas.receta)
-        router.push('/favoritos')
-        
+    }
+
+    function eliminarFavorito() {
+        favoritos.value = favoritos.value.filter(favorito => favorito.idDrink !== bebidas.receta.idDrink)
+    }
+    function handleClickFavorito() {
+        (existeFavorito(bebidas.receta.idDrink)) ? eliminarFavorito() : agregarFavorito()
+        modal.modal = false
     }
 
     return {
         favoritos,
-        handleClickFavorito
+        handleClickFavorito,
+        existeFavorito,
     }
 })
--- src/stores/modal.js
+++ src/stores/modal.js
@@ -1,17 +1,28 @@
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { useFavoritosStore } from './favoritos' 
+import { useBebidasStore } from './bebidas'
+
 
 export const useModalStore = defineStore('modal', () => {
 
+    const favoritos = useFavoritosStore()
     const modal = ref(false)
+    const bebidas = useBebidasStore()
 
     function handleClickModal() {
         modal.value = !modal.value
     }
 
+    const textoBoton = computed(() => {
+        if(modal.value) {
+            return favoritos.existeFavorito(bebidas.receta.idDrink) ? 'Eliminar de favoritos' : 'Añadir a favoritos'
+        }
+    })
 
     return {
         modal,
+        textoBoton,
         handleClickModal,
     }
 })
